090b18c6c76399c8ebda603f16912f8723ea9994
Ahn-TaeKyung <154008745+Ahn-TaeKyung@users.noreply.github.com>
2025-05-04 16:24:16 +0900

    submit complete

--- src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
+++ src/main/java/com/rookies3/myspringbootlab/MySpringbootLabApplication.java
@@ -1,13 +1,21 @@
 package com.rookies3.myspringbootlab;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringbootLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MySpringbootLabApplication.class, args);
+//		SpringApplication.run(MySpringBootAppApplication.class, args);
+
+		SpringApplication application = new SpringApplication(MySpringbootLabApplication.class);
+		//기본적으로 WebApplicationType 은 웹어플리케이션
+		application.setWebApplicationType(WebApplicationType.SERVLET);
+		application.run(args);
+
 	}
 
 }
